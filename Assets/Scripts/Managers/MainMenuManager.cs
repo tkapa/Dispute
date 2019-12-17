93b940280730a6c6ef34486d98700e6d45bf9f9d
Kapa <tylahkapa@gmail.com>
2019-12-17 10:39:46 +1000

    Started Main Menu

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera mainCamera = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            OpenOptionsMenu();
+        } else if (Input.GetKeyDown(KeyCode.P)){
+            ReturnToMainMenu();
+        }
+    }
+
+    public void PlayGame(){
+        //TODO: Create First level and put the player into it.
+        //DESIREABLE: Make a save feature so that people can save? Play should take 10 mins tops
+    }
+
+    public void OpenOptionsMenu(){
+        mainCamera.Priority = 9;
+    }
+
+    public void ReturnToMainMenu(){
+        mainCamera.Priority = 11;
+    }
+}
