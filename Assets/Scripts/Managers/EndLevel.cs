3a9df7b86075fa482976d3cd318855f84a0d110e
Kapa <tylahkapa@gmail.com>
2019-12-20 21:43:12 +1000

    Player can now complete the level. Should place some restrictions on it like killing all enemies. Maybe it varies per level?

---
+++ Assets/Scripts/Managers/EndLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")){
+            Debug.Log("Level Complete");
+            FindObjectOfType<PauseManager>().LevelComplete();
+        }
+    }
+}
