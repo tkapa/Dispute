10227063ab35089a1cf52a247c32c3e0ee7d3ed5
Kapa <tylahkapa@gmail.com>
2019-12-24 14:16:15 +1000

    Working on limiting sprint and representing sprint

--- Assets/Scripts/Player/PlayerSprint.cs
+++ Assets/Scripts/Player/PlayerSprint.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSprint : MonoBehaviour
 {
     public float sprintTime = 6.0f;
     private float sprintTimer = 0.0f;
+
+    public float sprintCooldownTime = 3.0f;
+    private float sprintCooldownTimer = 0.0f;
+
+    public Slider sprintSlider = null;
+
+    bool canSprint = true;
 
     PlayerMovement movement = null;
 
