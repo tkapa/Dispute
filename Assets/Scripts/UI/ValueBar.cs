51d94e4ee4d902c21e9c71c22381e45414783f27
Kapa <tylahkapa@gmail.com>
2019-12-24 15:47:36 +1000

    Created health bar

---
+++ Assets/Scripts/UI/ValueBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueBar : MonoBehaviour
+{
+    public Transform barTransform = null;
+
+    public void SetValue(float normalisedValue){
+        barTransform.localScale = new Vector3(normalisedValue, 1f);
+    }
+}
