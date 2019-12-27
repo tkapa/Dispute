163af2fdedd235ec9192619e35250b83c528961c
Kapa <tylahkapa@gmail.com>
2019-12-27 21:59:21 +1000

    Game Audio now also goes down as stress goes up. Dev tools now let me control stress resetting or going up to 50%. When the player reaches 100% stress, the game will automatically close.

--- Assets/Scripts/Managers/DevTools.cs
+++ Assets/Scripts/Managers/DevTools.cs
@@ -8,6 +8,8 @@ public class DevTools : MonoBehaviour
 
     public Weapon weapon = null;
 
+    public SOFloat stress = null;
+
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +19,14 @@ public class DevTools : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.I)){
             weapon.infiniteAmmo = !weapon.infiniteAmmo;
+        }
+
+        if(Input.GetKeyDown(KeyCode.J)){
+            stress.value = 0.0f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.K)){
+            stress.value = 50.0f;
         }
     }
 }
--- Assets/Scripts/Player/PlayerStress.cs
+++ Assets/Scripts/Player/PlayerStress.cs
@@ -10,6 +10,7 @@ public class PlayerStress : MonoBehaviour
 
     public ValueBar stressBar = null;
     public AudioMixer gameMixer = null;
+    public SOFloat gameSound = null;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +19,30 @@ public class PlayerStress : MonoBehaviour
     }
 
     private void Update() {
-        StressIncrease(Time.deltaTime*0.5f);
+        StressIncrease(Time.deltaTime*2f);
     }
 
     public void StressIncrease(float value){
         stress.value += value;
         SetStressValues();
+
+        if(stress.value >= maximumStress){
+            Cursor.lockState = CursorLockMode.None;
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
     }
 
     void SetStressValues(){
         float percent = stress.value/maximumStress;
         stressBar.SetValue(percent);
 
-        gameMixer.SetFloat("disputeVolume", (90*percent)+-70);
-        //gameMixer.SetFloat("gameAudioVolume", 20 - (90*percent));
+        gameMixer.SetFloat("disputeVolume", (60*percent)+-50);
+
+        float newMusicValue = (70*(gameSound.value/100)) - ((70*(gameSound.value/100))*percent);
+        gameMixer.SetFloat("gameAudioVolume", -70+newMusicValue);
     }
 }
